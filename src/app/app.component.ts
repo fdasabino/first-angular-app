1645da188a7ad2f11a4e6a53aa3fd2b703f0ecd2
Francisco Sabino <fdasabino@gmail.com>
2024-05-29 16:20:09 +0200

    chore: Add user component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/UI/footer/footer.component';
 import { HeaderComponent } from './components/UI/header/header.component';
+import { UserComponent } from './components/user/user.component';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, UserComponent],
     templateUrl: './app.component.html',
 })
 export class AppComponent {}
