b3e24869980226ba8143740b84ea3fbf0cab1f9e
Francisco Sabino <fdasabino@gmail.com>
2024-05-29 16:52:02 +0200

    chore: Update user and main components with grid layout

---
+++ data/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Jasmine Washington',
+        avatar: 'https://plus.unsplash.com/premium_photo-1675034393378-df55b8d93459?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGphc21pbmV8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+        id: 'u2',
+        name: 'Emily Thompson',
+        avatar: 'https://images.unsplash.com/photo-1543207564-1271b510019d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZW1pbHl8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+        id: 'u3',
+        name: 'Marcus Johnson',
+        avatar: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80',
+    },
+    {
+        id: 'u4',
+        name: 'David Miller',
+        avatar: 'https://images.unsplash.com/photo-1593283590172-adfce2adf213?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGF2aWR8ZW58MHx8MHx8fDA%3D',
+    },
+    {
+        id: 'u5',
+        name: 'Priya Patel',
+        avatar: 'https://images.unsplash.com/photo-1664575602554-2087b04935a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tYW58ZW58MHx8MHx8fDA%3D',
+    },
+    {
+        id: 'u6',
+        name: 'Arjun Singh',
+        avatar: 'https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aW5kaWFufGVufDB8fDB8fHww',
+    },
+];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DUMMY_USERS } from '../../data/dummy-users';
 import { FooterComponent } from './components/UI/footer/footer.component';
 import { HeaderComponent } from './components/UI/header/header.component';
 import { UserComponent } from './components/user/user.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, UserComponent],
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        FooterComponent,
+        UserComponent,
+        CommonModule,
+    ],
     templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+    users = DUMMY_USERS;
+}
